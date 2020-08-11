a1aef406481cfc43234118d6bb52d81e5d3e7fc8
Raíssa L. S. Silva <r.lorenna@gmail.com>
2020-08-11 19:33:20 -0300

    Update

--- src/getTableFeatures.R
+++ src/getTableFeatures.R
@@ -1,8 +1,6 @@
 #############################################################################################
 ####################### Extract Features fom CDS and intergenics orfs #######################
 #############################################################################################
-
-registerDoParallel(cl)
 #print("Extract features")
 print("Extracting the features")
 tableFeatures<- getFeatures(tableSequences)
