7c0ec9a14e22c99acc9712bb3213a2ddd8cb77bc
Raíssa L. S. Silva <r.lorenna@gmail.com>
2021-11-22 17:30:17 +0100

    changes in the output

--- src/predicting.R
+++ src/predicting.R
@@ -4,6 +4,9 @@ load(modelFile)
 
 #custom <- readRDS('./src/model.RData')
 
+contigsId <- as.data.frame(tableFeatures$contig)
+tableFeatures <- tableFeatures[,-12]
+
 #dataset
 closeAllConnections()
 
@@ -24,17 +27,25 @@ tableSequencesNO <- tableSequences[which(tableSequences$predictClass == "no") ,]
 folder_and_name = paste0(id,"_genes.fasta")
 
 tableSeq <- data.frame(tableSequencesYES)
+name_seqs <- paste0(tableSeq$seq,", len=", tableSeq$tamanho,", ", tableSeq$contig)
 tableSeq <- setNames(split(tableSeq[,1], seq(nrow(tableSeq))), rownames(tableSeq))
-write.fasta(sequences = tableSeq, names = names(tableSeq), nbchar = 80, file.out = folder_and_name)
+write.fasta(sequences = tableSeq, names = name_seqs, nbchar = 80, file.out = folder_and_name)
 
 if(non == 1){
   print("Completed")
 }
 if(non == 2){
+  folder_and_name = paste0(id,"_genes.fasta")
+  tableSeq <- data.frame(tableSequencesYES)
+  name_seqs <- paste0(tableSeq$seq,", len=", tableSeq$tamanho,", ", tableSeq$contig)
+  tableSeq <- setNames(split(tableSeq[,1], seq(nrow(tableSeq))), rownames(tableSeq))
+  write.fasta(sequences = tableSeq, names = name_seqs, nbchar = 80, file.out = folder_and_name)
+  
   folder_and_name = paste0(id,"_intergenics.fasta")
   tableSeq <- data.frame(tableSequencesNO)
+  name_seqs <- paste0(tableSeq$seq,", len=", tableSeq$tamanho,", ", tableSeq$contig)
   tableSeq <- setNames(split(tableSeq[,1], seq(nrow(tableSeq))), rownames(tableSeq))
-  write.fasta(sequences = tableSeq, names = names(tableSeq), nbchar = 80, file.out = folder_and_name)
+  write.fasta(sequences = tableSeq, names = name_seqs, nbchar = 80, file.out = folder_and_name)
   print("Completed")
 }
 
