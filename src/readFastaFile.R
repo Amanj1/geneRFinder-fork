41a8468575dc73d85c2b4a4a60adabef40e345e2
Raíssa L. S. Silva <r.lorenna@gmail.com>
2021-11-22 17:30:59 +0100

    changes in the output

--- src/readFastaFile.R
+++ src/readFastaFile.R
@@ -1,19 +1,24 @@
 sequencesFile <- NULL
-sequencesFile <-  paste0(read.fasta(fileDir, seqtype = "DNA", as.string = TRUE, forceDNAtolower = FALSE))
+sequencesFile <-  paste0(seqinr::read.fasta(fileDir, seqtype = "DNA", as.string = TRUE, forceDNAtolower = FALSE))
 
 sequencesTable <- NULL
 sequencesTable <- data.frame(matrix(unlist(sequencesFile), nrow=length(sequencesFile), byrow=T),stringsAsFactors=FALSE)
 
+library(phylotools)
+contigsNames <- as.data.frame(get.fasta.name(fileDir))
+colnames(contigsNames) <- "contig"
+               
 closeAllConnections()
 
 colnames(sequencesTable) <- c("sequencia")
 
 print("Reading the fasta file")
 tableSequencesPred <- data.frame(sequencia = NA, classe = NA, tamanho = NA, sentido = NA, org = NA, seq = NA)
-tableSequencesPred <- cbind(sequencesTable, tableSequencesPred)
+tableSequencesPred <- cbind(sequencesTable, tableSequencesPred, contigsNames)
 
 tableSequences <- findOrfs(tableSequencesPred, "cds", TRUE, startC)
 
 rm(sequencesFile)
 rm(sequencesTable)
 rm(tableSequencesPred)
+rm(contigsNames)
